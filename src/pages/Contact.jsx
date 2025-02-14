4ce5ce5413831de157718bc6f4da3c7b8e6e7aba
VictorD <victor.demessance@gmail.com>
2025-02-14 18:41:21 +0100

    Responsive Contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,7 +6,7 @@ import { theme } from "../theme";
 import emailjs from "emailjs-com"; // Assurez-vous que emailjs est bien installé
 
 const Section = styled.div`
-  height: 92vh;
+  height: 70vh;
   scroll-snap-align: center;
   display: flex;
   flex-direction: column;
@@ -24,7 +24,7 @@ const SecondRow = styled.div`
   background: ${theme.gradient.secondary};
   color: white;
   font-weight: bold;
-  padding-block: 1rem;
+  padding-block: 2vh;
   flex: 0.1;
   display: flex;
   flex-direction: column;
@@ -34,17 +34,21 @@ const SecondRow = styled.div`
 `;
 
 const FirstColumn = styled.div`
-  flex: 1;
+  flex: 0.4;
   display: flex;
   flex-direction: column;
   justify-content: center;
-  gap: 200px;
   align-items: center;
+  gap: 20vh;
+
+  @media (max-width: 768px) {
+    display: none;
+  }
 `;
 
 const IconNetwork = styled.div`
-  height: 100px;
-  width: 100px;
+  width: min(10vw, 10vh);
+  aspect-ratio: 1 / 1; 
   cursor: pointer;
   background-image: url(${props => props.url});
   background-size: contain;
@@ -58,25 +62,24 @@ const IconNetwork = styled.div`
 `;
 
 const SecondColumn = styled.div`
+  flex: 0.6;
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: flex-start;
-  flex: 1;
+  align-items: center;
 `;
 
 const ContactContainer = styled.div`
-  padding-block: 2rem;
+  padding-block: 4vh;
   border-radius: 30px;
   height: fit-content;
-  width: 500px;
+  width: 40vw;
   background: ${theme.colors.primary};
   border: 1px solid rgba(180, 180, 180, 0.12);
   box-shadow: #00479015 0px 0px 1rem 1rem;
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
-  gap: 20px;
   align-items: center;
   animation: floating 3s infinite ease alternate;
 
@@ -85,67 +88,91 @@ const ContactContainer = styled.div`
       transform: translateY(10px);
     }
   }
+
+  @media (max-width: 768px) {
+    width: 80vw;
+    padding-block: 3vh;
+  }
 `;
 
 const Form = styled.form`
   width: 90%;
   display: flex;
   flex-direction: column;
-  gap: 15px;
+  gap: 1.5vh;
   align-items: start;
   padding-inline: 20px;
 `;
 
 const Label = styled.label`
-  font-size: 18px;
+  font-size: 1.5vw;
+
+  @media (max-width: 768px) {
+    font-size: 1.5vh;
+  }
 `;
 
 const Input = styled.input`
-  font-size: 18px;
+  font-size: 1.5vw;
   color: black;
   font-weight: bold;
   background: ${theme.colors.secondary_transparent};
   width: 100%;
-  padding: 10px;
+  padding-block: 2vh;
+  padding-inline: 1vw;
   border: 0px;
   border-radius: 5px;
+
+  @media (max-width: 768px) {
+    font-size: 1.5vh;
+  }
 `;
 
 const TextArea = styled.textarea`
   font-weight: bold;
-  font-size: 15px;
+  font-size: 1.5vw;
   color: black;
   width: 100%;
   background: ${theme.colors.secondary_transparent};
-  height: 120px;
-  padding: 10px;
+  height: 15vh;
+  padding-block: 2vh;
+  padding-inline: 1vw;
   border: 0px;
   border-radius: 5px;
   resize: none;
+
+  @media (max-width: 768px) {
+    font-size: 1.5vh;
+  }
 `;
 
 const Select = styled.select`
   font-weight: bold;
   background: ${theme.colors.secondary_transparent};
-  font-size: 18px;
+  font-size: 1.5vw;
   cursor: pointer;
   width: 100%;
-  padding: 10px;
+  padding-block: 2vh;
+  padding-inline: 1vw;
   border: 0px;
   border-radius: 5px;
   color: black;
-  margin-bottom: 15px;
+  margin-bottom: 2vh;
+
+  @media (max-width: 768px) {
+    font-size: 1.5vh;
+  }
 `;
 
 const Button = styled.button`
-  margin-top: 10px;
-  padding: 10px 20px;
+  margin-top: 1vh;
+  padding: 1vh 1.5vw;
   color: white;
   font-weight: bold;
   border-radius: 10px;
   cursor: pointer;
   align-self: center;
-  font-size: 20px;
+  font-size: 2vw;
   border: 0px;
   background: ${theme.gradient.secondary};
   transition: scale 0.1s ease-in-out;
@@ -157,6 +184,10 @@ const Button = styled.button`
   &:active {
     background: ${theme.gradient.secondary_transparent};
     scale: 1;
+  }
+
+  @media (max-width: 768px) {
+    font-size: 2vh;
   }
 `;
 
